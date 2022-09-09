221d4a217844cd9d398b036a748e0702b563dd5b
ainkne <thminh195sn@gmail.com>
2022-09-09 09:52:44 +0700

    fix enhanceBubbleSort input

--- src/Display.java
+++ src/Display.java
@@ -1,17 +1,19 @@
 import java.util.Arrays;
 public class Display {
     public static void displayArray(){
-        int[] arrayOfInt = RandomArray.generateRandomArray();
-        System.out.println("Unsorted array: " + Arrays.toString(arrayOfInt));
+        int[] arrayOfInt1 = RandomArray.generateRandomArray();
+        System.out.println("Unsorted array: " + Arrays.toString(arrayOfInt1));
         
         long start = System.nanoTime();
-        BubbleSort.bubbleSort(arrayOfInt);
+        BubbleSort.bubbleSort(arrayOfInt1);
         long end = System.nanoTime() - start;
-        System.out.println("Sorted array using bubble sort: " + Arrays.toString(arrayOfInt) + "| Take: " + end + "ns");
+        System.out.println("Sorted array using bubble sort: " + Arrays.toString(arrayOfInt1) + "| Take: " + end + "ns");
         //
+        int[] arrayOfInt2 = RandomArray.generateRandomArray();
+        System.out.println("Unsorted array: " + Arrays.toString(arrayOfInt1));
         long start_enhance = System.nanoTime();
-        BubbleSort.enhanceBubbleSort(arrayOfInt);
+        BubbleSort.enhanceBubbleSort(arrayOfInt2);
         long end_enhance = System.nanoTime() - start_enhance;
-        System.out.println("Sorted array using enhance bubble sort: " + Arrays.toString(arrayOfInt) + "| Take: " + end_enhance + "ns");
+        System.out.println("Sorted array using enhance bubble sort: " + Arrays.toString(arrayOfInt2) + "| Take: " + end_enhance + "ns");
     }
 }
